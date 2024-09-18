a67c9d6fe06acf81d118b67732bd2633350c3cc0
tnlsmswnd <jkok3847@naver.com>
2024-09-18 21:33:51 +0900

    Add files via upload

---
+++ 0912/0912 gamepgm3_화면 지우기.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <windows.h>
+int main(void)
+{
+	char ch;
+
+    printf("문자를 입력하고 Enter>");
+	scanf("%c", &ch);
+	system("cls");
+	printf("입력된 문자 %c\n", ch);
+
+    return 0;
+}
